e90c8700e22290c4b2dccb0db493aa950f0abeb1
Nero_Li <375892799@qq.com>
2020-10-23 13:40:32 +0800

    #完善一些

---
+++ foodie-dev-api/src/main/java/com/lyming/controller/BaseController.java
@@ -0,0 +1,26 @@
+package com.lyming.controller;
+
+import com.lyming.pojo.Orders;
+import com.lyming.utils.IMOOCJSONResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.io.File;
+
+/**
+ * description:
+ *
+ * @author lyming
+ * @date 2020/9/15 1:22 上午
+ */
+@Controller
+public class BaseController {
+    public static final String FOODIE_SHOPCART = "shopcart";
+
+    public static final Integer COMMON_PAGE_SIZE = 10;
+    public static final Integer PAGE_SIZE = 20;
+
+
+
+
+}
